5163e65b70d80a572a2d2bcf0faa12b670464597
Joaquin <joaquin.medina513@ciencias.unam.mx>
2024-10-14 22:40:17 -0600

    Se agregaron comentarios de las funciones

--- src/parser.clj
+++ src/parser.clj
@@ -1,9 +1,12 @@
 (ns parser
   (:require [grammars :as wae]))
 
+;; Para esto, se debe de considerar el primer elemento de la lista, si es
+;; un valor atomico o se debe de realizar recursión con respecto a la estructura
+;; del lenguaje
 (defn parser-AE
-  "Función que realiza el parser "
-  [exp] 
+  "Función que realiza el parser del lenguaje AE de un listado de variables"
+  [exp]
   (cond
     (number? exp)
     (wae/numG exp)
@@ -14,7 +17,11 @@
       - (wae/subG (parser-AE (second exp)) (parser-AE (nth exp 2)))
       :else (throw (IllegalArgumentException. "Syntax Error")))))
 
+
+;; Para esto, se debe de considerar los valores atomicos del lenguaje
+;; con respecto a la cabeza de la lista, sino realizar recursión
 (defn parser-WAE
+  "Función que realiza el parser del lenguaje WAE de un listado de variables"
   [exp]
   (cond
     (symbol? exp)
@@ -32,6 +39,5 @@
       with (wae/withG
             (wae/bindings (first (first (rest exp))) (parser-WAE (first (rest (first (rest exp))))))
             (parser-WAE (nth exp 2)))
-      :else (throw (IllegalArgumentException. "Syntax Error"))
-      )))
+      :else (throw (IllegalArgumentException. "Syntax Error")))))
 
