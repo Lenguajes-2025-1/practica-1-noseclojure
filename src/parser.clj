80afaae0c25fe5e01bc877e513936446e7fee884
Joaquin <joaquin.medina513@ciencias.unam.mx>
2024-10-14 22:36:21 -0600

    Implementación correcta del archivo parser.clj

--- src/parser.clj
+++ src/parser.clj
@@ -2,11 +2,36 @@
   (:require [grammars :as wae]))
 
 (defn parser-AE
-    "Por implementar"
-    [exp]
-    ())
+  "Función que realiza el parser "
+  [exp] 
+  (cond
+    (number? exp)
+    (wae/numG exp)
+
+    (list? exp)
+    (case (first exp)
+      + (wae/addG (parser-AE (second exp)) (parser-AE (nth exp 2)))
+      - (wae/subG (parser-AE (second exp)) (parser-AE (nth exp 2)))
+      :else (throw (IllegalArgumentException. "Syntax Error")))))
 
 (defn parser-WAE
-    "Por implemntar"
-    [exp]
-    ())
+  [exp]
+  (cond
+    (symbol? exp)
+    (wae/idG exp)
+
+    (number? exp)
+    (wae/numG exp)
+
+    (list? exp)
+    (case (first exp)
+      + (wae/addG (parser-WAE (second exp)) (parser-WAE (nth exp 2)))
+
+      - (wae/subG (parser-WAE (second exp)) (parser-WAE (nth exp 2)))
+
+      with (wae/withG
+            (wae/bindings (first (first (rest exp))) (parser-WAE (first (rest (first (rest exp))))))
+            (parser-WAE (nth exp 2)))
+      :else (throw (IllegalArgumentException. "Syntax Error"))
+      )))
+
